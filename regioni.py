819680d268f73c6bb3d41261f59c3475e1193c7f
Marco Sampietro <sampietro.marco@iisgalvanimi.edu.it>
2024-05-16 09:49:57 +0000

    Aggiornamento

---
+++ regioni.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import pandas as pd
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('search.html')
+
+@app.route('/search', methods = ['GET'])
+def search():
+    regione = request.args['regione']
+    dati_regioni = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
+    risultato = dati_regioni[dati_regioni['denominazione_regione']==regione.capitalize()].index
+    if len(risultato) == 0:
+        table = 'Regione non trovata'
+    else:
+        table = list(risultato)
+    return render_template('radiobutton.html', tabella = table)
+
+@app.route('/info', methods = ['GET'])
+def info():
+    regione = request.args['id']
+    dati_regioni = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
+    risultato = dati_regioni.loc[regione]
+    if len(risultato) == 0:
+        table = 'Regione non trovata'
+    else:
+        table = risultato.to_html()
+    return render_template('tabella.html', tabella = table)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
